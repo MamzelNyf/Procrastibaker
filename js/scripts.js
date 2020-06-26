4b63c25a8e4f5cec17e286d4ede563a3d1da2b35
MamzelNyf <fanny@shedesign.dev>
2020-06-27 00:24:16 +0800

    midnight commit - navigation- js

--- js/scripts.js
+++ js/scripts.js
@@ -4,5 +4,17 @@ function changeColour() {
     document.documentElement.style.setProperty('--light', '#c866cc');
     document.documentElement.style.setProperty('--white', '#fbecf0');
     document.documentElement.style.setProperty('--accent', '#ff8124');
-
 }
+
+const navToggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelectorAll('.nav__link');
+
+navToggle.addEventListener('click', () => {
+    document.body.classList.toggle('nav-open');
+});
+
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        document.body.classList.remove('nav-open');
+    })
+});
