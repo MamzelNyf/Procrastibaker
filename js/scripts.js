2ce82a3b755fdf44b3240f7128bc7ffae2c1e109
MamzelNyf <fanny@shedesign.dev>
2020-12-02 15:38:02 +0800

    update projects and bread anim

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,22 @@
+let sheCodes = false;
+
 function changeColour() {
-    document.documentElement.style.setProperty('--dark', '#320b3d');
-    document.documentElement.style.setProperty('--medium', '#591d69');
-    document.documentElement.style.setProperty('--light', '#c866cc');
-    document.documentElement.style.setProperty('--white', '#fbecf0');
-    document.documentElement.style.setProperty('--accent', '#ff8124');
+    // console.log(sheCodes)
+    if (sheCodes === false) {
+        document.documentElement.style.setProperty('--dark', '#320b3d');
+        document.documentElement.style.setProperty('--medium', '#591d69');
+        document.documentElement.style.setProperty('--light', '#c866cc');
+        document.documentElement.style.setProperty('--white', '#fbecf0');
+        document.documentElement.style.setProperty('--accent', '#ff8124');
+        sheCodes = true
+    } else {
+        document.documentElement.style.setProperty('--dark', '#373A53');
+        document.documentElement.style.setProperty('--medium', '#6b72af');
+        document.documentElement.style.setProperty('--light', '#91caae');
+        document.documentElement.style.setProperty('--white', '#fff');
+        document.documentElement.style.setProperty('--accent', '#EC8C83');
+        sheCodes = false
+    }
 }
 
 const navToggle = document.querySelector('.nav-toggle');
